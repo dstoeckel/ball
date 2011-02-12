719723716cedb89cfb6e8d21d48abe16b9c636ec
Wolfgang Herget <wolfgang@r007.de>
2011-02-12 12:49:04 +0100

    fragmentXMLFile: add method signatures

--- include/BALL/FORMAT/fragmentXMLFile.h
+++ include/BALL/FORMAT/fragmentXMLFile.h
@@ -10,15 +10,39 @@
 #endif
 
 namespace BALL {
-        /** Fragment XML file class.
-            Support for reading fragmentXML files, one variant at a time.
-            \ingroup StructureFormats
-        */
-        class BALL_EXPORT FragmentXMLFile
-                  : public GenericMolFile
-        {
+	/** Fragment XML file class.
+		Support for reading fragmentXML files, one variant at a time.
+		\ingroup StructureFormats
+	*/
+	class BALL_EXPORT FragmentXMLFile
+				: public GenericMolFile
+	{
+		public:
 
-        };
+		/**	@name	Constructors and Destructors
+		*/
+		//@{
+		/// Default constructor.
+		FragmentXMLFile();
+		/// Detailed constructor.
+		FragmentXMLFile(const String& filename, File::OpenMode open_mode = std::ios::in);
+		/// Destructor.
+		virtual ~FragmentXMLFile();
+		//@}
+
+		/// Read a single molecule, in this case the first variant in the file.
+		Molecule* read();
+
+		/// Read the whole System, aka all variants from the file.
+		bool read(System& system);
+
+		/// Read a specific molecule (variant) from the file.
+		Molecule* read(const String& variantName);
+
+		FragmentXMLFile& operator = (const GenericMolFile& rhs);
+
+		// (no need to reimplement operators, they call read/write in the superclass)
+	};
 }
 
 #endif // BALL_FORMAT_FRAGMENTXMLFILE_H
