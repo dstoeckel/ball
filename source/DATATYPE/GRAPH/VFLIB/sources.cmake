a6045e0b67055ec0b1006e8ab9c9d489a9dd2391
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2011-07-12 17:11:07 +0200

    Disable the build of VFLib sources
    We are currently porting them to boost. This requires
    templatization and thus the sources should be unnecessary.

--- source/DATATYPE/GRAPH/VFLIB/sources.cmake
+++ source/DATATYPE/GRAPH/VFLIB/sources.cmake
@@ -1,24 +1,24 @@
 ### list all filenames of the directory here ###
 SET(SOURCES_LIST
-argedit.C
-argloader.C
-argraph.C
-error.C
-gene.C
-gene_mesh.C
-match.C
-sd_state.C
-sortnodes.C
-ull_state.C
-ull_sub_state.C
-vf2_mcs_state.C
-vf2_mono_state.C
-vf2_state.C
-vf2_sub_state.C
-vf_mono_state.C
-vf_state.C
-vf_sub_state.C
-xsubgraph.C
+#argedit.C
+#argloader.C
+#argraph.C
+#error.C
+#gene.C
+#gene_mesh.C
+#match.C
+#sd_state.C
+#sortnodes.C
+#ull_state.C
+#ull_sub_state.C
+#vf2_mcs_state.C
+#vf2_mono_state.C
+#vf2_state.C
+#vf2_sub_state.C
+#vf_mono_state.C
+#vf_state.C
+#vf_sub_state.C
+#xsubgraph.C
 )
 
 ADD_BALL_SOURCES("DATATYPE/GRAPH/VFLIB" "${SOURCES_LIST}")
