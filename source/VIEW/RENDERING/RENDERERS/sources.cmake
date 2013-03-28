df7b99ae9c6e078962d198dd797c6c99f637aa5c
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2013-03-28 19:16:22 +0100

    Merge Renderer, BufferedRenderer and RaytracingRenderer
    We create one fat interface that will be refactored later on.

--- source/VIEW/RENDERING/RENDERERS/sources.cmake
+++ source/VIEW/RENDERING/RENDERERS/sources.cmake
@@ -1,12 +1,10 @@
 ### list all filenames of the directory here ###
 SET(SOURCES_LIST
-	glRenderer.C
 	renderer.C
 	POVRenderer.C
 	STLRenderer.C
 	tilingRenderer.C
 	VRMLRenderer.C
-	raytracingRenderer.C
 	XML3DRenderer.C
 )
 
