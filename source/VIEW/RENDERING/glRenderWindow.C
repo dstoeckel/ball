5364b7796574d6c9e74a4adc010e311570eeffc5
Daniel Stoeckel <dstoeckel@bioinf.uni-sb.de>
2013-03-28 19:32:49 +0100

    Extend the RenderTarget interface to be more generic

--- source/VIEW/RENDERING/glRenderWindow.C
+++ source/VIEW/RENDERING/glRenderWindow.C
@@ -215,6 +215,19 @@ namespace BALL
 			glPopAttrib();
 		}
 
+		bool GLRenderWindow::doNotResize() const
+		{
+			return RenderWindow::doNotResize() || isFullScreen();
+		}
+
+		void GLRenderWindow::prepareRendering()
+		{
+			if (QGLContext::currentContext() != context())
+			{
+				makeCurrent();
+			}
+		}
+
 		void GLRenderWindow::renderText(int x, int y, const String& text, const ColorRGBA& color, Size size)
 		{
 			glMatrixMode(GL_PROJECTION);
@@ -366,7 +379,7 @@ namespace BALL
 		{
 			if(isVisible() && getMainControl()->windowHandle()->isExposed())
 			{
-				swapBuffers();
+				QGLWidget::swapBuffers();
 			}
 		}
 	} // namespace VIEW
